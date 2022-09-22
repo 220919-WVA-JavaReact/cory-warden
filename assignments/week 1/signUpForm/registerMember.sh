4f83b74f12fa5ccf50eef60da7f7c47165d17f52
Cory W <12cwarden@gmail.com>
2022-09-22 17:26:45 -0400

    comp to restart script

--- assignments/week 1/signUpForm/registerMember.sh
+++ assignments/week 1/signUpForm/registerMember.sh
@@ -2,36 +2,36 @@
 
 echo "Membership Status? (existing/new)";
 read status;
-
+			
 #if member is new, proceed to signup form.
 if [[ $status = "new" ]]
   then
-  echo "Welcome! Please answer a few questions below."
-  read -p "What is your first name?" fname; 
+  echo "Welcome! Please answer a few questions below.";
+  read -p "What is your first name?" fname;
   read -p "What is your last name?" lname;
   read -p "What is your email?" email;
   read -p "What is your username?" user;
   read -p "What is your password?" pw;
-  echo "You have now signed up!"
-  echo "$fname,$lname,$email,$user,$pw" >> mockdata.csv #push saved variables into mockdata in csv format
-  exit 0;
+  echo "You have now signed up!";
+  echo "$fname,$lname,$email,$user,$pw" >> mockdata.csv; #push saved variables into mockdata in csv format
+  echo "Now try to login!";
+  bash ./registerMember.sh;
 fi
-
+			
 # if member is existing, show current information for member.
 if [[ $status = "existing" ]]
   then
   read -p "What is your name? " name;
 # parse information received and send back file report.
   sh parser.sh mockdata.csv | grep -A5 "First Name: $name$";
-else 
+else
 #if not member, return reason for no information. Potentially utilize wrap to beginning?
   read -p "you are not an existing member. Try again from the beginning? (y/n)" resp;
   if [[ $resp = "y" ]]
    then
-   exec ./${0};
+   bash ./registerMember.sh
   else
    echo "Closing program."
   fi
 fi
-
 
