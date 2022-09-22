1d14f455017f44f388a8d5db8a7159c9a407ff3f
Cory W <12cwarden@gmail.com>
2022-09-22 17:02:30 -0400

    barebones version of signup

---
+++ assignments/week 1/signUpForm/parser.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+exec < $1
+read header #read/skip first line of csv file.
+
+while IFS="," read -r fname lname email user pw
+do
+	echo "First Name: $fname";
+	echo "Last Name: $lname";
+	echo "Email: $email";
+	echo "Username: $user";
+	echo "Password: $pw";
+	echo "+---------------------------------------+";
+done
+
---
+++ assignments/week 1/signUpForm/registerMember.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Membership Status? (existing/new)";
+read status;
+
+#if member is new, proceed to signup form.
+if [[ $status = "new" ]]
+  then
+  echo "Welcome! Please answer a few questions below."
+  read -p "What is your first name?" fname; 
+  read -p "What is your last name?" lname;
+  read -p "What is your email?" email;
+  read -p "What is your username?" user;
+  read -p "What is your password?" pw;
+  echo "You have now signed up!"
+  echo "$fname,$lname,$email,$user,$pw" >> mockdata.csv #push saved variables into mockdata in csv format
+  exit 0;
+fi
+
+# if member is existing, show current information for member.
+if [[ $status = "existing" ]]
+  then
+  read -p "What is your name? " name;
+# parse information received and send back file report.
+  sh parser.sh mockdata.csv | grep -A5 "First Name: $name$";
+else 
+#if not member, return reason for no information. Potentially utilize wrap to beginning?
+  read -p "you are not an existing member. Try again from the beginning? (y/n)" resp;
+  if [[ $resp = "y" ]]
+   then
+   exec ./${0};
+  else
+   echo "Closing program."
+  fi
+fi
+
+
