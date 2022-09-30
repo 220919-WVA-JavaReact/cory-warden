f16f40fc13669eaa28f82e7eca77286b17921c56
Cory W <12cwarden@gmail.com>
2022-09-30 18:44:52 -0400

    Fixed looping functionality

--- assignments/week 2/java/JavaFundamentals/src/main/java/com/revature/calculator/SimpleCalculator.java
+++ assignments/week 2/java/JavaFundamentals/src/main/java/com/revature/calculator/SimpleCalculator.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class SimpleCalculator {
     //create main for run functionality
     public static void main(String[] args) {
-        //store two numbers with decimal potential and operator sign for user to input as well as initialize scanner
-        String restart = "yes";
-        Scanner input = new Scanner(System.in);
-        int num1;
-        int num2;
-        char oper;
 
-        while (restart == "yes") {
+        //store two numbers with decimal potential and operator sign for user to input as well as initialize scanner
+        Scanner input = new Scanner(System.in);
+        boolean active = true;
+        float num1;
+        float num2;
+        char oper;
+;
+        while (active) {
             //intake all input values below
             System.out.println("Simple Calculator");
             System.out.println("---------------------------------------------");
             System.out.println("Please enter the first number you would like to add");
-            num1 = input.nextInt();
+            num1 = input.nextFloat();
 
             System.out.println("Please enter the operator you would like to use (+ - / *)");
             oper = input.next().charAt(0);
 
             System.out.println("Please enter the second number you would like to add");
-            num2 = input.nextInt();
+            num2 = input.nextFloat();
+            input.nextLine();
 
             //create result variable, start switch case based upon operator choice
             float res = 0;
@@ -56,14 +58,14 @@ public class SimpleCalculator {
             }
             System.out.println("Your answer is: " + String.format("%.2f", res));
             System.out.println("---------------------------------------------");
-
-            System.out.println("If you would like to quit, press 'q'");
+            System.out.println("Would you like to run again? (y/n)");
             String resp = input.nextLine();
 
-            if (resp != "yes") {
-                break;
+            if (resp.equals("y")) {
+                active = true;
+            } else {
+                System.out.println("Thank you for using the calculator!");
             }
-
         }
     }
 }
